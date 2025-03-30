5d571a151db53b6fb0a27534dc1472594b6aa9ea
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2025-03-30 15:47:35 +0800

    fix: forgot to change API_BASE

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-//export const API_BASE = 'https://shoti.fbbot.org/api';
+export const API_BASE = 'https://shoti.fbbot.org/api';
 
-export const API_BASE = 'http://localhost:5000/api';
+//export const API_BASE = 'http://localhost:5000/api';
